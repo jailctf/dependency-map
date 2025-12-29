d0d89028cd03d1840361bcc406bdc2fbd2ab39ca
quasar098 <70716985+quasar098@users.noreply.github.com>
2025-12-29 03:55:38 -0500

    epic betterness

--- generate_website.py
+++ generate_website.py
@@ -1,6 +1,40 @@
 import os
 import shutil  # probably should have been using this but whatever
 import json
+import re
+
+
+def sort_version(version_s):
+    split = version_s.removesuffix('.json').split('.')
+    total = []
+    for w in split:
+        try:
+            total.append(int(w))
+        except:
+            total.append(w)
+    if isinstance(total[-1], str):
+        last = total.pop(-1)
+        last_better = []
+        if 'a' in last:
+            last_split = last.split('a')
+            last_better = [int(last_split[0]), 0, int(last_split[1])]
+        elif 'b' in last:
+            last_split = last.split('b')
+            last_better = [int(last_split[0]), 1, int(last_split[1])]
+        elif 'rc' in last:
+            last_split = last.split('rc')
+            last_better = [int(last_split[0]), 2, int(last_split[1])]
+        else:
+            raise NotImplementedError("bad version for sorting: " + version_s)
+        total.extend(last_better)
+    else:
+        while len(total) < 3:
+            total.append(3)
+        if len(total) == 3:
+            total.extend([3, 0])
+        if len(total) == 4:
+            total.append(0)
+    return tuple(total)
 
 
 def gen_website(inp):
@@ -26,7 +60,7 @@ def main():
         exit(1)
     else:
         json_exports = [f for f in os.listdir(os.path.join('exports', 'json')) if f.endswith('.json')]
-    json_exports = list(sorted(json_exports, reverse=True))
+    json_exports = list(sorted(json_exports, reverse=True, key=sort_version))
     print('export files')
     print('=' * 60)
     print('\n'.join('* ' + fname for fname in json_exports[:15]))
@@ -53,6 +87,20 @@ def main():
             inp = json_exports[0]
             gen_website(inp)
             break
+    print('generating index.html')
+    with open('index-template.html', 'r') as f:
+        index_html = f.read()
+    with open(os.path.join('exports', 'web', 'index.html'), 'w') as f:
+        per_version_htmls = {}
+        for version in json_exports:
+            version_split = sort_version(version)
+            if version_split[:2] not in per_version_htmls:
+                per_version_htmls[version_split[:2]] = []
+            per_version_htmls[version_split[:2]].append(f'<a href="{version.removesuffix(".json")}.html" target="_blank">{version.removesuffix(".json")}</a>')
+        pages_html = '<br>'.join(sorted(['<p>' + ', '.join(per_version_htmls[v]) + '</p>' for v in per_version_htmls], reverse=True))
+        index_html = index_html.replace('{!{PAGESHTML}!}', pages_html)
+        f.write(index_html)
+    print('successfully generated index.html')
 
 
 if __name__ == '__main__':
